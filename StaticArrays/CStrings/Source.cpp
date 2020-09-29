89818ee5246b9fc71bf14e69c9452cd1905dfebf
thedeepestreality <zetoster@gmail.com>
2020-09-29 18:54:05 +0300

    fixed getline read issue

--- StaticArrays/CStrings/Source.cpp
+++ StaticArrays/CStrings/Source.cpp
@@ -7,6 +7,7 @@ int main()
 	char hell[] = { 'h','e','l','l' };
 	std::cout << str << '\n';
 	std::cout << hell << '\n';
+	//modify str variabe through memory access
 	hell[13] = 'W';
 	std::cout << str << '\n';
 	char str_copy[12];
@@ -19,9 +20,16 @@ int main()
 	//std::cout << str_copy[6] << std::endl;
 
 	char sentence[64];
-	//std::cin.getline(sentence, 10);
-	//std::cout << sentence << std::endl;
-
+	std::cin.getline(sentence, 10);
+	std::cout << sentence << std::endl;
+	//check if string was more than 10 symbols
+	//and read the rest
+	if (std::cin.fail())
+	{
+		std::cin.clear();
+		std::cin.getline(sentence, 10);
+	}
+	//read until ',' symbol is met
 	std::cin.getline(sentence, 10, ',');
 	std::cout << sentence << std::endl;
 	
