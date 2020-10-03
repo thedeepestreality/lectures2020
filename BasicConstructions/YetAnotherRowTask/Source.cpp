fb55952c570ce6e7089c4b3e18402dab979be482
thedeepestreality <zetoster@gmail.com>
2020-10-03 12:07:11 +0300

    yet another series example

---
+++ BasicConstructions/YetAnotherRowTask/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	double x1 = 0.5;
+	double x2 = 1.5;
+	double dx = 0.1;
+	const double magic = 1;
+	double eps = 0.1;
+	while (x1 < x2)
+	{
+		std::cout << x1;
+		if (abs(x1 - magic) >= eps)
+			std::cout << " magic!";
+		std::cout << '\n';
+		x1 += dx;
+	}
+	system("pause");
+	return 0;
+}
