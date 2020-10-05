7ae66a8e42322fcb136246579d39f0814d658040
thedeepestreality <zetoster@gmail.com>
2020-10-05 15:58:33 +0300

    added comments to DynamicMemory solution

--- DynamicMemory/Pointers/Source.cpp
+++ DynamicMemory/Pointers/Source.cpp
@@ -23,17 +23,25 @@ int main()
 	std::cout << "y: " << y << '\n';
 	int arr[5] = { 1,2,3,4,5 };
 	int* p_arr = arr;
-	*(p_arr + 1) = 10;
-	*(arr + 3) = 20;
+	*(p_arr + 1) = 10; // equal: p_arr[1] = 10;
+	*(arr + 3) = 20; //equal: arr[3] = 20;
+	//equal expressions
+	std::cout << arr[1] << std::endl;
 	std::cout << 1[arr] << std::endl;
-	std::cout << arr[3] << std::endl;
-	char* pc = (char*)&x;
-	x = 769;
+	std::cout << *(arr+1) << std::endl;
+
+	//decompose int varaible
+	//to the array of 4 chars
+	int val = 769;
+	//get addres of val and cast to char pointer
+	char* pc = (char*)&val;
 	std::cout << (int)pc[0] << '\n';
 	std::cout << (int)pc[1] << '\n';
 	std::cout << (int)pc[2] << '\n';
 	std::cout << (int)pc[3] << '\n';
 
+	//reverse operation
+	//compose int variable from 4-char array
 	char c_arr[4] = { 1,3,0,0 };
 	int* int_from_char = (int*)c_arr;
 	std::cout << *int_from_char << '\n';
