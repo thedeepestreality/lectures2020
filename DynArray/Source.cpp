31228baa61756a4d0b92969886c434571cfe3a10
thedeepestreality <zetoster@gmail.com>
2021-02-13 13:56:12 +0300

    13.02.21: more about classes

--- DynArray/Source.cpp
+++ DynArray/Source.cpp
@@ -15,10 +15,12 @@ void init(DynArrayStruct& arr, const size_t kSize)
 class DynArray
 {
 private:
-    int* _data;
     size_t _size;
+    int* _data;
+    //SomeClass obj;
+    size_t const _kMaxSize = 200;
 
-    inline void checkIdx(size_t const kIdx)
+    inline void checkIdx(size_t const kIdx) const
     {
         if (kIdx >= _size)
             throw "out of bounds";
@@ -30,17 +32,46 @@ public:
     //    _size = kSize;
     //    _data = new int[_size];
     //}
-    DynArray(size_t const kSize)
+
+    //Constructor with arguments
+    explicit DynArray(size_t const kSize) : _size(kSize),
+                                            _data(new int[_size]),
+                                            _kMaxSize(100)
+    {
+        std::cout << "Ya rodilsia!\n";
+    }
+
+    DynArray(size_t const kSize, int const kToFill)
     {
         std::cout << "Ya rodilsia!\n";
         _size = kSize;
         _data = new int[_size];
+        for (size_t idx = 0; idx < _size; ++idx)
+            _data[idx] = kToFill;
     }
 
+    DynArray(size_t const kSize, int values[])
+    {
+        std::cout << "Ya rodilsia!\n";
+        _size = kSize;
+        _data = new int[_size];
+        for (size_t idx = 0; idx < _size; ++idx)
+            _data[idx] = values[idx];
+    }
+
+    //Default constructor
     DynArray()
     {
         _size = 0;
         _data = nullptr;
+    }
+    //DynArray() = default;
+    //DynArray() = delete;
+
+    //Copy constructor
+    DynArray(DynArray const& source) : DynArray(source._size, source._data)
+    {
+        std::cout << "Copy constructor\n";
     }
 
     ~DynArray()
@@ -50,13 +81,14 @@ public:
             delete[] _data;
     }
 
-    size_t getSize()
+    size_t getSize() const
     {
+        getAt(2);
         return _size;
     }
 
     //Variant 1
-    int getAt(size_t const kIdx)
+    int getAt(size_t const kIdx) const
     {
         checkIdx(kIdx);
         return _data[kIdx];
@@ -69,8 +101,18 @@ public:
     }
 
     //Variant 2
+    // For const objects
+    int at(size_t const kIdx) const
+    {
+        std::cout << "I'm const\n";
+        checkIdx(kIdx);
+        return _data[kIdx];
+    }
+
+    // For not const objects
     int& at(size_t const kIdx)
     {
+        std::cout << "I'm not const\n";
         checkIdx(kIdx);
         return _data[kIdx];
     }
@@ -93,6 +135,19 @@ void dummy_func()
         dyn_arr.at(idx) = idx;
     for (size_t idx = 0; idx < kSize; ++idx)
         std::cout << dyn_arr.at(idx) << "\n";
+    
+    //HACKERMAN STUFF (USE WISELY (BETTER NOT USE AT ALL))
+  /*  ((size_t*)&dyn_arr)[0] = 2;
+    ((int**)&dyn_arr)[1][0] = 3;
+    
+    std::cout << "HACKERMAN!!11:\n";
+    std::cout << dyn_arr.getSize() << "\n";
+    std::cout << dyn_arr.at(0) << "\n";*/
+}
+
+void explicit_example(DynArray arr)
+{
+
 }
 
 int main()
@@ -105,7 +160,21 @@ int main()
     for (size_t idx = 0; idx < arr_struct.size; ++idx)
         arr_struct.data[idx] = idx;
 
-    //DynArray dyn_arr;
+    DynArray dyn_arr;
     dummy_func();
+
+    explicit_example(DynArray(3));
+
+    DynArray arr(3);
+    //...fill(arr)
+    DynArray arr_copy = arr; //DynArray arr_copy(arr); //DynArray arr_copy = DynArray(arr);
+    //DynArray arr_copy;
+    //arr_copy = arr;
+
+    DynArray const const_arr(3);
+
+    std::cout << const_arr.getSize() << "\n";
+    const_arr.at(1);
+
     return 0;
 }
